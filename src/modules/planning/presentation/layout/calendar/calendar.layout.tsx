0ecfcd03084d1ca123bb0effab53abc954ebac6c
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-05-05 00:23:28 +0200

    fix tooltip

--- src/modules/planning/presentation/layout/calendar/calendar.layout.tsx
+++ src/modules/planning/presentation/layout/calendar/calendar.layout.tsx
@@ -4,26 +4,42 @@ import { MissionDto } from "../../../domain/dto/Mission.dto.ts";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 
 export const CalendarLayout = ({ missions }: { missions: MissionDto[] }) => {
+    let index = 0;
+
+    const filteredMissions = missions.filter(mission => mission.date);
+
+    const sortedMissions = filteredMissions.sort((a, b) => {
+        const dateA = new Date(a.date!);
+        const dateB = new Date(b.date!);
+
+        if (dateA.getTime() === dateB.getTime()) {
+            return missions.indexOf(b) - missions.indexOf(a);
+        }
+        return dateA.getTime() - dateB.getTime();
+    });
+
+    const tooltips = sortedMissions.map((mission, index) => (
+        <ReactTooltip
+            id={`my-tooltip-${index}`}
+            key={index}
+            className={"z-20"}
+            place={"top"}
+            content={mission.description}
+        />
+    ));
 
     return (
         <div className="calendar-container">
             <FullCalendar
                 plugins={[dayGridPlugin]}
                 initialView="dayGridMonth"
-                events={missions.map(({id, ...rest}) => rest)}
+                events={missions.map(({ id, ...rest }) => rest)}
                 eventDidMount={(arg) => {
-                    arg.el.setAttribute("data-for", `my-tooltip-${arg.event.id}`);
+                    arg.el.dataset.tooltipId = `my-tooltip-${index}`;
+                    index++;
                 }}
             />
-            {
-                missions.map((mission, index) => (
-                    <ReactTooltip
-                        id={`my-tooltip-${index}`}
-                        place="bottom"
-                        content={mission.description}
-                    />
-                ))
-            }
+            {tooltips}
         </div>
     );
 };
